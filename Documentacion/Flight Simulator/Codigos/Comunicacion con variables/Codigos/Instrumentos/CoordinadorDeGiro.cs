1fa430086048a838e5787cb9e045ee4500bd0358
HarderDaddy <83604897+SantyLeiva@users.noreply.github.com>
2024-10-17 08:35:16 -0300

    Create CoordinadorDeGiro.cs

---
+++ Documentacion/Flight Simulator/Codigos/Comunicacion con variables/Codigos/Instrumentos/CoordinadorDeGiro.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.FlightSimulator.SimConnect;
+using System;
+using System.Runtime.InteropServices;
+
+class CoordinadorDeGiro
+{
+    private static SimConnect simconnect = default!;
+
+    public void ConectarSimConnect()
+    {
+        try
+        {
+            simconnect = new SimConnect("SimvarWatcher", IntPtr.Zero, 0x0402, null, 0);
+
+            simconnect.OnRecvSimobjectData += Simconnect_OnRecvSimobjectData;
+
+            simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "DELTA HEADING RATE", "degrees per second", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+            simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "TURN COORDINATOR BALL", "position", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+            simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "TURN INDICATOR RATE", "units", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+
+            simconnect.RegisterDataDefineStruct<Struct1>(DEFINITIONS.Struct1);
+
+            simconnect.RequestDataOnSimObject(DATA_REQUESTS.REQUEST_1, DEFINITIONS.Struct1, SimConnect.SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD.SECOND, SIMCONNECT_DATA_REQUEST_FLAG.DEFAULT, 0, 0, 0);
+        }
+        catch (COMException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+
+    private void Simconnect_OnRecvSimobjectData(SimConnect sender, SIMCONNECT_RECV_SIMOBJECT_DATA data)
+    {
+        var turnData = (Struct1)data.dwData[0];
+        Console.WriteLine($"Delta Heading Rate: {turnData.DeltaHeadingRate} grados por segundo");
+        Console.WriteLine($"Turn Coordinator Ball: {turnData.TurnCoordinatorBall}");
+        Console.WriteLine($"Turn Indicator Rate: {turnData.TurnIndicatorRate} unidades");
+    }
+
+    enum DATA_REQUESTS { REQUEST_1 }
+    enum DEFINITIONS { Struct1 }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    struct Struct1
+    {
+        public double DeltaHeadingRate;
+        public double TurnCoordinatorBall;
+        public double TurnIndicatorRate;
+    }
+}
