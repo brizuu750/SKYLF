9d6694a68a62cef31c9b206288f0afda07ac2d6b
SantiagoRubio <santy201205@gmail.com>
2024-10-20 18:24:51 -0300

    Mejorar Altimetro.cs

--- Documentacion/Flight Simulator/Codigos/Comunicacion con variables/Codigos/Instrumentos/Altimetro.cs
+++ Documentacion/Flight Simulator/Codigos/Comunicacion con variables/Codigos/Instrumentos/Altimetro.cs
@@ -1,4 +1,4 @@
-using Microsoft.FlightSimulator.SimConnect;
+﻿using Microsoft.FlightSimulator.SimConnect;
 using System;
 using System.Runtime.InteropServices;
 
@@ -11,7 +11,6 @@ class Altimetro
         try
         {
             simconnect = new SimConnect("SimvarWatcher", IntPtr.Zero, 0x0402, null, 0);
-
             simconnect.OnRecvSimobjectData += Simconnect_OnRecvSimobjectData;
 
             simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "INDICATED ALTITUDE", "feet", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
@@ -23,15 +22,38 @@ class Altimetro
         }
         catch (COMException ex)
         {
-            Console.WriteLine("Error: " + ex.Message);
+            Console.WriteLine("Error al conectar SimConnect en Altimetro: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error inesperado en Altimetro: " + ex.Message);
+        }
+    }
+
+    public void ReceiveMessage()
+    {
+        try
+        {
+            simconnect?.ReceiveMessage();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error al recibir mensaje en Altimetro: " + ex.Message);
         }
     }
 
     private void Simconnect_OnRecvSimobjectData(SimConnect sender, SIMCONNECT_RECV_SIMOBJECT_DATA data)
     {
-        var altitudeData = (Struct1)data.dwData[0];
-        Console.WriteLine($"Altitud indicada: {altitudeData.IndicatedAltitude} pies");
-        Console.WriteLine($"Ajuste Kollsman: {altitudeData.KollsmanSettingHG} inHg");
+        try
+        {
+            var altitudeData = (Struct1)data.dwData[0];
+            Console.WriteLine($"Altitud indicada: {altitudeData.IndicatedAltitude} pies");
+            Console.WriteLine($"Ajuste Kollsman: {altitudeData.KollsmanSettingHG} inHg");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error al procesar datos del altímetro: " + ex.Message);
+        }
     }
 
     enum DATA_REQUESTS { REQUEST_1 }
