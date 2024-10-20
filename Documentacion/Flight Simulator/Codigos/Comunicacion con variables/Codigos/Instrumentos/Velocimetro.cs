daf2c139b7332410ad609045df84f2dd67fe5a60
SantiagoRubio <santy201205@gmail.com>
2024-10-20 18:30:22 -0300

    Update Velocimetro.cs

--- Documentacion/Flight Simulator/Codigos/Comunicacion con variables/Codigos/Instrumentos/Velocimetro.cs
+++ Documentacion/Flight Simulator/Codigos/Comunicacion con variables/Codigos/Instrumentos/Velocimetro.cs
@@ -11,7 +11,6 @@ class Velocimetro
         try
         {
             simconnect = new SimConnect("SimvarWatcher", IntPtr.Zero, 0x0402, null, 0);
-
             simconnect.OnRecvSimobjectData += Simconnect_OnRecvSimobjectData;
 
             simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "AIRSPEED INDICATED", "knots", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
@@ -22,14 +21,37 @@ class Velocimetro
         }
         catch (COMException ex)
         {
-            Console.WriteLine("Error: " + ex.Message);
+            Console.WriteLine("Error al conectar SimConnect en Velocímetro: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error inesperado en Velocímetro: " + ex.Message);
+        }
+    }
+
+    public void ReceiveMessage()
+    {
+        try
+        {
+            simconnect?.ReceiveMessage();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error al recibir mensaje en Velocímetro: " + ex.Message);
         }
     }
 
     private void Simconnect_OnRecvSimobjectData(SimConnect sender, SIMCONNECT_RECV_SIMOBJECT_DATA data)
     {
-        var speedData = (Struct1)data.dwData[0];
-        Console.WriteLine($"Velocidad indicada: {speedData.IndicatedAirspeed} nudos");
+        try
+        {
+            var speedData = (Struct1)data.dwData[0];
+            Console.WriteLine($"Velocidad indicada: {speedData.IndicatedAirspeed} nudos");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error al procesar datos del velocímetro: " + ex.Message);
+        }
     }
 
     enum DATA_REQUESTS { REQUEST_1 }
@@ -41,3 +63,4 @@ class Velocimetro
         public double IndicatedAirspeed;
     }
 }
+
